89ae1ef849f0329aa9a339de520a791b0be7a4d1
Clement Denis <le.mikmac@gmail.com>
2016-12-19 14:44:08 +0100

    init phenomic

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(cat package.json | jq '.repository')}
+
+cd dist && \
+$(npm bin)/rimraf .git
+git init && \
+git add . && \
+git commit -m "Deploy to GitHub Pages" && \
+git push --force $GIT_DEPLOY_REPO master:gh-pages
+
